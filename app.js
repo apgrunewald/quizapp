00bb03bfc5526fb11fbc1876f0e0b3804cc17887
apgrunewald <apgrunewald@gmail.com>
2016-01-17 13:24:30 -0800

    -all

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('#howButton').click(function () {
+	      //alert("button is working");
+	      $("#intro").show();
+	      $('#howButton').hide();
+	});
+	$('#play').click(function () {
+	      //alert("button is working");
+	      $("#intro").hide();
+	});
+});
